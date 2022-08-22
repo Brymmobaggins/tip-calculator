725570c028bb2b54a9b46c7c126dc0252a349be6
Brymmobaggins <ibro2tablet21@yahoo.com>
2022-08-22 11:54:10 +0100

    add javascript file

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const resetButton = document.getElementById('reset')
 const peopleInput = document.getElementById('people')
 const customInput = document.getElementById('custom')
-const amount = document.querySelector('#fiqure')
-// const para = document.querySelector('span')
+const tipAmount = document.querySelector('#amount')
+const tipTotal = document.querySelector('#total')
+// // const para = document.querySelector('span')
+// const coys = document.getElementById('coys')
 
 function checkInput() {
     if (peopleInput.value < 1) {
@@ -10,7 +12,12 @@ function checkInput() {
     }
 }
 
+
+// reset button
 resetButton.onclick = function () {
-    // const amount = document.querySelector('#fiqure')
-    amount.textContent = 0.00
+    tipAmount.textContent = "0.00"
+    tipTotal.textContent = "0.00"
+    customInput.value = "0"
+    peopleInput.value = "1"
+
 }
